20088bd40ae36632ba0c02a447b7c30a30d0f2b0
Alex <Agutierr22@jaumebalmes.net>
2023-02-08 19:18:46 +0100

    mainA

---
+++ main.py
@@ -0,0 +1,11 @@
+import exercici1_A
+import exercici2_A
+import exercici3_A
+
+if exercici1_A == exercici1_A:
+    # trucada a la funció del exercici 1_A
+    exercici1_A.func1()
+    # trucada a la funció del exercici 2_A
+    exercici2_A.func2()
+    # trucada a la funció del exercici 3_A
+    exercici3_A.func3()
